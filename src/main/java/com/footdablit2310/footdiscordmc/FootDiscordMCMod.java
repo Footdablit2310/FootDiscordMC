71a066467d2751447fbda7c690a9547cb82e3845
Footdablit2310 <kolayparamita@gmail.com>
2026-08-11 11:34:56 +0200

    Fixed event timing

--- src/main/java/com/footdablit2310/footdiscordmc/FootDiscordMCMod.java
+++ src/main/java/com/footdablit2310/footdiscordmc/FootDiscordMCMod.java
@@ -1,9 +1,11 @@
 package com.footdablit2310.footdiscordmc;
 
 import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.EventPriority;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.common.NeoForge;
@@ -17,17 +19,15 @@ import javax.net.ssl.SSLContext;
 public class FootDiscordMCMod {
     public static final String MOD_ID = "footdiscordmc";
     public static final Logger LOGGER = LogUtils.getLogger();
-    public FootDiscordMCMod(IEventBus modBus) {
-        modBus.addListener(this::commonSetup);
+    public FootDiscordMCMod(ModContainer modContainer) {
+        modContainer.registerConfig(ModConfig.Type.SERVER, FootConfigHandler.CONFIG_SPEC);
+        modContainer.getEventBus().addListener(this::commonSetup);
 
         // Register this class on the global event bus
         NeoForge.EVENT_BUS.register(this);
     }
-    public FootDiscordMCMod(ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.SERVER, FootConfigHandler.CONFIG_SPEC);
-    }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
+    private void commonSetup(ModConfigEvent.Loading event) {
         assert FootConfigHandler.CONFIG.getSSL() != null;
         FootDiscordHTTPUtil.init(!FootConfigHandler.CONFIG.getSSL().get());
 
